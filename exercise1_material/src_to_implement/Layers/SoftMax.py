320c7985e263bda939f1e6319c9ce6c13bed03e6
ahmedhat1 <ahmed.sadekibrahim@gmail.com>
2023-05-18 23:03:03 +0200

    Softmax done

--- exercise1_material/src_to_implement/Layers/SoftMax.py
+++ exercise1_material/src_to_implement/Layers/SoftMax.py
@@ -4,23 +4,14 @@ class SoftMax(BaseLayer):
     def __init__(self):
         super().__init__()
     def forward(self, input_tensor):
-        zeros_tensor = np.zeros_like(input_tensor)
-        # if np.greater(input_tensor, zeros_tensor).any():
-        if np.any(input_tensor > 0 ):
-            input_tensor = np.subtract(input_tensor, np.max(input_tensor))
-        exp_tensor = np.exp(input_tensor)
-        try:
-            #exp_tensor.shape[1]
-            den = np.sum(exp_tensor, axis = 0)
-            den = np.repeat(np.expand_dims(den,axis=1),exp_tensor.shape[0],axis=1)
-            self.output_tensor = np.divide(exp_tensor,den.T)
-            return self.output_tensor
-        except:
-            self.output_tensor = np.divide(exp_tensor,np.sum(exp_tensor))
-            #print("out_tensor", np.sum(self.output_tensor))
-            return self.output_tensor
-        # output = exp_tesnor[:,]
-    def backward(self, error_tensor):
-        pass
-        #return error_tensor_prev
+        maximum = np.expand_dims(np.max(input_tensor, 1), 1) 
+        x_tilda = input_tensor - maximum # to increase stability
+        numerator = np.exp(x_tilda) 
+        denominator = np.expand_dims(np.sum(numerator, axis=1,), 1)
+        self.output_tensor = numerator / denominator
+        return self.output_tensor
+    def backward(self, label_tensor):
+        scalar_rows = np.sum(np.multiply(label_tensor,self.output_tensor), axis=1)
+        error = np.multiply(self.output_tensor, np.subtract(label_tensor[0:label_tensor.shape[0], :].T, scalar_rows).T)
+        return error
 
