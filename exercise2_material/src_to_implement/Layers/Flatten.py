5ce89cc5fc2b7eea022ce833c09a13405dfd21d3
ibrahimam <amgad.ibrahim@trumpf.com>
2023-06-09 22:34:27 +0200

    Flatten

---
+++ exercise2_material/src_to_implement/Layers/Flatten.py
@@ -0,0 +1,15 @@
+import numpy as np
+from Layers.Base import BaseLayer
+
+class Flatten(BaseLayer):
+    def __init__(self):
+        super().__init__()
+    def forward(self, input_tensor):
+        # Getting the shape of the input tensor
+        self.shape = input_tensor.shape
+        # When reshaping, we must take into consideration the batch size (self.shape[0])
+        return np.array(input_tensor).reshape(self.shape[0], np.prod(self.shape[1:]))
+    def backward(self, error_tensor):
+        # reshaping the error tesnor again to the original shape
+        return np.array(error_tensor).reshape(self.shape)
+
