ab0479db2d1d2de2e559658e393ed5c7ba57d69d
ibrahimam <amgad.ibrahim@trumpf.com>
2023-06-13 13:56:21 +0200

    conv

--- exercise2_material/src_to_implement/Layers/Conv.py
+++ exercise2_material/src_to_implement/Layers/Conv.py
@@ -1,7 +1,7 @@
 from Layers.Base import BaseLayer
 import numpy as np
 from scipy import signal
-
+from scipy.ndimage import convolve
 class Conv(BaseLayer):
     def __init__(self, stride_shape, convolution_shape, num_kernels):
         super().__init__()
@@ -22,7 +22,7 @@ class Conv(BaseLayer):
         self.m = self.convolution_shape[1]
 
         # in case of 2D input
-        if (len(convolution_shape) == 3):
+        if (len(self.convolution_shape) == 3):
             self.img_2d = True
             self.n = self.convolution_shape[2]
             self.weights = np.random.uniform(0, 1, ((self.num_kernels, self.input_channel_num, self.m, self.n)))
@@ -32,11 +32,58 @@ class Conv(BaseLayer):
             self.bias = np.random.uniform(0, 1, (self.num_kernels))
     
     def forward(self, input_tensor):
-        pass
+        num_kernels = self.num_kernels
+        if not self.img_2d:
+            batch_size = input_tensor.shape[0]
+            input_size = input_tensor.shape[2]
+            filter_size = self.weights.shape[1]
+            # num_kernels = self.weights.shape[0]
+            output_size = (input_size - filter_size) // self.stride_shape[0] + 1
+
+            self.output = np.zeros((batch_size, num_kernels, output_size ))
+
+            for j in range(batch_size):
+                for i in range(self.output.shape[2]):
+                    for k in range(self.output.shape[1]):
+                        input_slice = input_tensor[j][:,i:i+filter_size]
+                        self.output[j][k][i] = np.sum(self.weights[k]* input_slice   )
+
+            return self.output
+        else:
+            input_height, input_width = input_tensor.shape[:2]
+            filter_height, filter_width = self.weights.shape[:2]
+            num_filters = self.weights.shape[0]
+            padding_height, padding_width = (0,0)
+            stride_height, stride_width = self.stride_shape
+
+            output_height = (input_height - filter_height + 2 * padding_height) // stride_height + 1
+            output_width = (input_width - filter_width + 2 * padding_width) // stride_width + 1
+
+            output = np.zeros((output_height, output_width, num_filters))
+
+            padded_input = np.pad(input_tensor, ((padding_height, padding_height), (padding_width, padding_width), (0, 0)), mode='constant')
+
+            for i in range(output_height):
+                for j in range(output_width):
+                    input_slice = padded_input[i*stride_height:i*stride_height+filter_height,
+                                            j*stride_width:j*stride_width+filter_width]
+                    output[i, j] = np.sum(input_slice * self.weights, axis=(0, 1))
+
+            return output
     
     def backward(self, error_tensor):
         pass
+    def initialize(self, weights_initializer, bias_initializer):
 
+        if (self.img_2d == True):
+            self.fan_in = self.input_channel_num * self.m * self.n
+            self.fan_out = self.num_kernels * self.m * self.n
+        else:
+            self.fan_in = self.input_channel_num * self.m
+            self.fan_out = self.num_kernels * self.m
+
+        self.weights = weights_initializer.initialize(self.weights.shape, self.fan_in, self.fan_out)
+        self.bias = bias_initializer.initialize(self.bias.shape, self.fan_in, self.fan_out)
     # @property
     # def gradient_weights(self):
     #     return self.gradient_w
